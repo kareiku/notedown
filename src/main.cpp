b43f6155dee0a7a167f3930a005378a52f2eabe6
kareiku <xaxvu7@gmail.com>
2025-06-07 17:43:01 +0200

    Start the main window maximized

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
     NotedownWindow window;
-    window.show();
+    window.showMaximized();
 
     return QApplication::exec();
 }
