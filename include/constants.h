430897502f84be55a3ccf3a5ec4e5ad194dffefc
kareiku <xaxvu7@gmail.com>
2025-06-08 00:22:32 +0200

    Added constants' file with organization and app name

---
+++ include/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#ifndef SETTINGS_ORG
+#define SETTINGS_ORG "io.github.kareiku"
+#endif
+
+#ifndef SETTINGS_APP
+#define SETTINGS_APP "notedown"
+#endif
+
+#endif
