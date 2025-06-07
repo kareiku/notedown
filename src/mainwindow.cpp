8843f70df75663a286991626294d534ddbd10790
kareiku <xaxvu7@gmail.com>
2025-06-08 00:23:19 +0200

    Add open and close events

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,4 +1,7 @@
-#include "mainwindow.h"
+#include <QSettings>
+
+#include "mainwindow.hpp"
+#include "constants.h"
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
@@ -6,4 +9,17 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::showEvent(QShowEvent *event) {
+    const QSettings settings(SETTINGS_ORG, SETTINGS_APP);
+    this->resize(settings.value("window/size", QSize(800, 600)).toSize());
+    this->move(settings.value("window/position", QPoint(100, 100)).toPoint());
+}
+
+void MainWindow::closeEvent(QCloseEvent *event) {
+    QSettings settings(SETTINGS_ORG, SETTINGS_APP);
+    settings.setValue("window/size", this->size());
+    settings.setValue("window/position", this->pos());
+    settings.sync();
 }
